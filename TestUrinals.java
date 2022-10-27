80b19ddc5bb38739d7f2e4cae315e05032978a05
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2022-10-27 10:03:12 -0700

    Added Unit Test for checking for appropriate character in buildString

--- TestUrinals.java
+++ TestUrinals.java
@@ -13,4 +13,12 @@ public class TestUrinals {
         urinals urinal = new urinals();
         Assertions.assertFalse(urinal.buildString("a"));
     }
+    /*The following tests the buildString method to see if it will
+     * properly return false when fed an inappropriate character.*/
+    @Test
+    public void testBuildStringProperCharacter() {
+        urinals urinal = new urinals();
+        Assertions.assertTrue(urinal.buildString("1"));
+        Assertions.assertTrue(urinal.buildString("0"));
+    }
 }
