da3f0355077eca33933c9a709020de99301564ad
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2022-10-27 10:05:54 -0700

    Added Unit Test for checking that appropriate string is built

--- TestUrinals.java
+++ TestUrinals.java
@@ -14,11 +14,20 @@ public class TestUrinals {
         Assertions.assertFalse(urinal.buildString("a"));
     }
     /*The following tests the buildString method to see if it will
-     * properly return false when fed an inappropriate character.*/
+     * properly return true when fed an appropriate character.*/
     @Test
     public void testBuildStringProperCharacter() {
         urinals urinal = new urinals();
         Assertions.assertTrue(urinal.buildString("1"));
         Assertions.assertTrue(urinal.buildString("0"));
+    }
+    /*The following tests the buildString method to see if it will
+     * properly build a string of 1's and 0's.*/
+    @Test
+    public void testBuildString() {
+        urinals urinal = new urinals();
+        urinal.buildString("1");
+        urinal.buildString("0");
+        Assertions.assertEquals("10", urinal.constructedString);
     }
 }
