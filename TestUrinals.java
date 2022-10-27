212229e2e8822d5dd6579aff5befd55a5806067b
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2022-10-27 15:13:25 -0700

    Added test to check that findMaxNumFromConstructedString can't process an empty string

--- TestUrinals.java
+++ TestUrinals.java
@@ -123,12 +123,19 @@ public class TestUrinals {
         Assertions.assertEquals(0, urinal.findMaxNumFromConstructedString());
     }
     /*The following tests the findMaxNumFromConstructedString method to see if it will
-     * produce the correct maximum number from the string of 1's and 0's that has adjacent occupancies*/
+     * produce the correct maximum number from the string of 1's and 0's that has adjacent occupancies.*/
     @Test
     public void testFindMaxNumFromConstructedStringWithOccupancyAdjacent() {
         urinals urinal = new urinals();
         urinal.buildString("1");
         urinal.buildString("1");
+        Assertions.assertEquals(-1, urinal.findMaxNumFromConstructedString());
+    }
+    /*The following tests the findMaxNumFromConstructedString method to see if it will
+     * produce the correct maximum number from the empty string.*/
+    @Test
+    public void testFindMaxNumFromConstructedStringEmpty() {
+        urinals urinal = new urinals();
         Assertions.assertEquals(-1, urinal.findMaxNumFromConstructedString());
     }
 }
